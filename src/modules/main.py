b762013e0cbb5ee3566b06826d59a4ff741c6072
Bogdan <igri228@gmail.com>
2026-09-01 14:02:04 +0400

    feat(coins): extract id, 24h change, and volume into coins list

--- src/modules/main.py
+++ src/modules/main.py
@@ -4,23 +4,23 @@ import rich
 def main():
     print("Начало программы")
 
-#    def retry(max_attemps=3, delay=2):
-#        pass
-
     api = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1'
 
     response = requests.get(api)
 
     data = response.json()
 
+    coins = []
+
     for coin in data:
-        id_val = coin['id']
-        change24percentage = coin['price_change_percentage_24h']
-        volume = coin['total_volume']
+        record = {
+            'id' : coin['id'],
+            'change24percentage' : coin['price_change_percentage_24h'],
+            'volume' : coin['total_volume']
+        }
+        coins.append(record)
 
-        print(id_val, change24percentage, volume)
-
-    print('Json row: ', data)
+    print(coins)
 
 if __name__ == "__main__":
     main()

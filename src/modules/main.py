a078638e9503796b9f422d7cb3a6a951aa8a48c6
Bogdan <igri228@gmail.com>
2026-09-05 13:23:44 +0400

    feat(spinner): show spinner while fetching crypto data

--- src/modules/main.py
+++ src/modules/main.py
@@ -1,9 +1,13 @@
+
+
 from api_request import fetch_coins_data
 from analysis import extract_coin_fields, get_top_gainers, get_top_losers, get_sum_market_cap, get_top_value_coin
-from design import gainers_losers_table
+from design import gainers_losers_table, console
 
 def main():
-    data = extract_coin_fields((fetch_coins_data()))
+
+    with console.status('Загрузка данных...'):
+        data = extract_coin_fields((fetch_coins_data()))
 
     gainers_losers_table(data)
 

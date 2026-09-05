3177ab8f8908ad2a5db5857ee77ccf3bb0ef2f87
Bogdan <igri228@gmail.com>
2026-09-05 14:31:10 +0400

    feat(report): implement report module for JSON export

--- src/modules/main.py
+++ src/modules/main.py
@@ -1,15 +1,14 @@
-
-
 from api_request import fetch_coins_data
 from analysis import extract_coin_fields, get_top_gainers, get_top_losers, get_sum_market_cap, get_top_value_coin
 from design import gainers_losers_table, console
-
+from  report import report_generator
 def main():
 
     with console.status('Загрузка данных...'):
         data = extract_coin_fields((fetch_coins_data()))
 
     gainers_losers_table(data)
+    report_generator(data)
 
 if __name__ == "__main__":
     main()
---
+++ src/modules/report.py
@@ -0,0 +1,17 @@
+import json
+import datetime
+
+from analysis import get_sum_market_cap, get_top_gainers, get_top_losers, get_top_value_coin
+
+def report_generator(data_coins):
+    report = {
+        'generated_at': datetime.datetime.now().isoformat(),
+        'total_coins_analyzed' : len(data_coins),
+        'total_market_cap_usd' : get_sum_market_cap(data_coins),
+        'top_gainers' : get_top_gainers(data_coins),
+        'top_losers' : get_top_losers(data_coins),
+        'highest_volume' : get_top_value_coin(data_coins)
+    }
+
+    with open('crypto_report.json', 'w', encoding='UTF8') as f:
+        f.write(json.dumps(report, indent=4))

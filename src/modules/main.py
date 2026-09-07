03a51c3967565ac1b449f98ad36d9e0b7a78a8b5
Bogdan <igri228@gmail.com>
2026-09-07 20:03:21 +0400

    for checking

--- src/modules/main.py
+++ src/modules/main.py
@@ -4,17 +4,13 @@ from design import gainers_losers_table, console
 from  report import report_generator
 def main():
 
-    #запрос к api и возвращаем нужные поля в data
+    # Кирилл привет!
+
     with console.status('Загрузка данных...'):
         data = extract_coin_fields((fetch_coins_data()))
 
-    print('Джисон с нужными полями: ', data)
-
     #Таблица
     gainers_losers_table(data)
-
-    #Создаём отчёт
-    report_generator(data)
 
 if __name__ == "__main__":
     main()

87e11e713f071216d0684cd76c5726b802d7381d
Bogdan <igri228@gmail.com>
2026-09-23 13:55:05 +0400

    refactor(main): adapt main to OOP architecture

--- src/modules/main.py
+++ src/modules/main.py
@@ -1,26 +1,31 @@
-from modules.api_request import fetch_coins_data
-from modules.analysis import extract_coin_fields, get_top_gainers, get_top_losers, get_top_value_coin, get_sum_market_cap
-from modules.design import gainers_losers_table, console
-from modules.report import report_generator, save_report
+from modules.api_request import APIRequest, API_URL, API_PARAMS
+from modules.analysis import GainersAnalysis, LosersAnalysis, TopValueAnalysis, MarketCapAnalysis
+from modules.output import ConsoleOutput, CsvOutput, JsonOutput
+from modules.report import Report
 
 def main():
 
     #запрос к api и возвращаем нужные поля в data
-    with console.status('Загрузка данных...'):
-        data = extract_coin_fields(fetch_coins_data())
+
+    request = APIRequest(API_URL, API_PARAMS)
+    data_coins = request.fetch_coins_data()
+
 
     # делаем анализ один раз
-    top_gainers = get_top_gainers(data)
-    top_losers = get_top_losers(data)
-    top_value_coin = get_top_value_coin(data)
-    market_cap = get_sum_market_cap(data)
+    top_gainers = GainersAnalysis(data_coins)
+    top_losers = LosersAnalysis(data_coins)
+    top_value_coin = TopValueAnalysis(data_coins)
+    market_cap = MarketCapAnalysis(data_coins)
 
-    #Таблица
-    gainers_losers_table(top_gainers, top_losers, top_value_coin, market_cap)
+    # Создаём отчёт
+    report = Report(data_coins, top_gainers.analyze(), top_losers.analyze(), market_cap.analyze(), top_value_coin.analyze())
 
-    #Создаём отчёт
-    report = report_generator(data, top_gainers, top_losers, market_cap, top_value_coin)
-    save_report(report)
+    #Ввывод:
+
+    # #таблица
+    console = ConsoleOutput(report)
+    console.output()
+
 if __name__ == "__main__":
     main()
 

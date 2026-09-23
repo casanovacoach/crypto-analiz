1520a569e130b97eaf0b80d0b82b1f9b52b14900
Bogdan <igri228@gmail.com>
2026-09-23 13:15:03 +0400

    refactor(design->Output): convert Output generation to OOP and add CSV output

--- src/modules/Output.py
+++ src/modules/Output.py
@@ -1,3 +1,4 @@
+import json
 from abc import abstractmethod, ABC
 
 from rich.console import Console
@@ -8,40 +9,50 @@ console = Console()
 
 class Output(ABC):
 
-    def __init__
+    def __init__(self, report):
+        self.report = report
 
     @abstractmethod
-    def output(self, data):
+    def output(self):
+        pass
 
-    def __init__(self, top_gainers, top_losers, top_value_coin, market_cap):
-        super().__init__()
-        self.top_gainers = top_gainers
-        self.top_losers = top_losers
-        self.top_value_coin = top_value_coin
-        self.market_cap = market_cap
 
-class OutputConsole(Output):
-    def output(self, data):
+class ConsoleOutput(Output):
+
+    def __init__(self, report):
+            super().__init__(report)
+
+    def output(self):
         table = Table(title='Топ Роста и Падения')
         table.add_column('Рост', justify='right', style='green')
         table.add_column('За 24 часа', justify='right', style='green')
         table.add_column('Падение', justify='left', style='red')
         table.add_column('За 24 часа', justify='left', style='red')
         # заполнение таблицы с переводом процентов(float) в string
-        for g, l in zip(self.top_gainers, self.top_losers):
+        for g, l in zip(self.report.top_gainers, self.report.top_losers):
             table.add_row(g['name'], f"{g['change24percentage']:.2f}%", l['name'], f"{l['change24percentage']:.2f}%")
 
         console.print(table)
-        console.print(f"Самый крупный по объёму торгов: [bold green]{self.top_value_coin['name']}[/bold green]")
-        console.print(f'Капитал рынка: [bold green]{self.market_cap}[/bold green]')
+        console.print(f"Самый крупный по объёму торгов: [bold green]{self.report.top_value_coin['name']}[/bold green]")
+        console.print(f'Капитал рынка: [bold green]{self.report.market_cap}[/bold green]')
 
-class JsonFormatter(OutputFormatter):
-    def output(self, data):
+class JsonOutput(Output):
+
+    def __init__(self, report):
+            super().__init__(report)
+
+
+
+    def output(self):
         with open('crypto_report.json', 'w', encoding='UTF8') as f:
-            json.dump(report, f, indent=4, ensure_ascii=False)
+            json.dump(self.report, f, indent=4, ensure_ascii=False)
 
 
-class CSVFormatter(OutputFormatter):
-    def output(self, data):
+class CsvOutput(Output):
+
+    def __init__(self, report):
+            super().__init__(report)
+
+    def output(self):
         with open('crypto_report.json', 'w', encoding='UTF8') as f:
-            json.dump(report, f, indent=4, ensure_ascii=False)
+            json.dump(self.report, f, indent=4, ensure_ascii=False)
